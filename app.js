e3ccd4d87a952a582328a20250ff35b0e3d22865
Muyiwa Akin-Ogundeji <oakinogundeji@gmail.com>
2017-04-27 22:14:23 +0100

    :package: implemented initial welcome message

--- app.js
+++ app.js
@@ -47,6 +47,29 @@ app.use(express.static(path.join(__dirname, 'public')));
  */
 //=============================================================================
 app.get('/test', (req, res) => res.status(200).json('ok!'));
+
+app.get('/', (req, res) => {
+    const options = {
+        root: __dirname,
+        dotfiles: 'deny'
+    };
+    return res.sendFile('index.html', options, err => {
+        if(err) {
+            console.error(`Couldn't send index.html`);
+            return res.status(500).json({error: err});
+        } else {
+            return console.log('Index.html sent');
+        }
+    });
+});
+
+app.get('/welcome', (req, res) => {
+  const welcomeMsg = [
+    "Hi, i'm Percy - your PERsonal Champion online. I'm a part of the Percayso family and i'm totally at your service.",
+    "I can give you more information about what we do at Percayso or i can help you get the best deals for anything you want using our proprietary technologies to leverage targeted cashback deals just for you!"
+  ];
+  return res.status(200).json({data: welcomeMsg});
+});
 //=============================================================================
 /**
  * bind server
